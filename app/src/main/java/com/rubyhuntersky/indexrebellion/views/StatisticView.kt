1c8e109b0aecd7633bf57a89bcf6f9ce7c14c95d
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-01-07 11:21:49 -0800

    Add StatisticView

---
+++ app/src/main/java/com/rubyhuntersky/indexrebellion/views/StatisticView.kt
@@ -0,0 +1,59 @@
+package com.rubyhuntersky.indexrebellion.views
+
+import android.content.Context
+import android.graphics.Color
+import android.support.annotation.AttrRes
+import android.support.annotation.ColorInt
+import android.support.annotation.StyleRes
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import com.rubyhuntersky.indexrebellion.R
+import kotlinx.android.synthetic.main.view_statistic.view.*
+
+class StatisticView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = 0,
+    @StyleRes defStyleRes: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.view_statistic, this)
+        attrs?.let { initAttributes(it, defStyleAttr, defStyleRes) }
+    }
+
+    private fun initAttributes(attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) {
+        context.theme.obtainStyledAttributes(attrs, R.styleable.StatisticViewAttrs, defStyleAttr, defStyleRes)
+            .apply {
+                try {
+                    text = getString(R.styleable.StatisticViewAttrs_text) ?:
+                            context.getString(R.string.statisticViewDefaultValue)
+                    labelText = getString(R.styleable.StatisticViewAttrs_labelText) ?:
+                            context.getString(R.string.statisticViewDefaultUnit)
+                    textColor = getColor(R.styleable.StatisticViewAttrs_textColor, Color.DKGRAY)
+                } finally {
+                    recycle()
+                }
+            }
+    }
+
+    var text: CharSequence
+        get() = this.valueTextView.text
+        set(value) {
+            this.valueTextView.text = value
+        }
+
+    var textColor: Int
+        get() = this.valueTextView.currentTextColor
+        set(@ColorInt value) {
+            this.valueTextView.setTextColor(value)
+            this.labelTextView.setTextColor(value)
+        }
+
+    var labelText: CharSequence
+        get() = this.labelTextView.text
+        set(value) {
+            this.labelTextView.text = value
+        }
+}
