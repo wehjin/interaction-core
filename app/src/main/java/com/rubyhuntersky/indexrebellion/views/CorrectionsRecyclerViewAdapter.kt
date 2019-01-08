c7e086926a3e927a6abc2243e7f70a78fad9f58c
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-01-08 13:47:49 -0800

    Move correction views into separate layout

--- app/src/main/java/com/rubyhuntersky/indexrebellion/views/CorrectionsRecyclerViewAdapter.kt
+++ app/src/main/java/com/rubyhuntersky/indexrebellion/views/CorrectionsRecyclerViewAdapter.kt
@@ -3,7 +3,6 @@ package com.rubyhuntersky.indexrebellion.views
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Space
 import com.rubyhuntersky.data.assets.AssetSymbol
 import com.rubyhuntersky.data.report.Correction
 import com.rubyhuntersky.indexrebellion.R
@@ -31,30 +30,14 @@ class CorrectionsRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHol
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val correctionsViewType = CorrectionsViewType.values()[viewType]
-        val view = when (correctionsViewType) {
-
-            CorrectionsViewType.HEADER -> LayoutInflater.from(parent.context).inflate(
-                R.layout.view_corrections_header,
-                parent,
-                false
-            )
-
-            CorrectionsViewType.CONNECTOR -> LayoutInflater.from(parent.context).inflate(
-                R.layout.view_corrections_connector,
-                parent,
-                false
-            )
-
-            CorrectionsViewType.FOOTER -> LayoutInflater.from(parent.context).inflate(
-                R.layout.view_corrections_footer,
-                parent,
-                false
-            )
-
-            CorrectionsViewType.CORRECTION -> Space(parent.context)
-
+        val layoutRes = when (correctionsViewType) {
+            CorrectionsViewType.HEADER -> R.layout.view_corrections_header
+            CorrectionsViewType.CONNECTOR -> R.layout.view_corrections_connector
+            CorrectionsViewType.FOOTER -> R.layout.view_corrections_footer
+            CorrectionsViewType.CORRECTION -> R.layout.view_corrections_correction
         }
-        return object : RecyclerView.ViewHolder(view) {}
+        val itemView = LayoutInflater.from(parent.context).inflate(layoutRes, parent, false)
+        return object : RecyclerView.ViewHolder(itemView) {}
     }
 
     override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {}
