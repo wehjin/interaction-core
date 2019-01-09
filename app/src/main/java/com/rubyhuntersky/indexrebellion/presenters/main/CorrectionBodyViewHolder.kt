5bf814e1014b4ebaef64b65c949fdf688d8928cf
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-01-09 03:30:50 -0800

    When a Hold correction has 0 weight, hide wings in correction body view.

--- app/src/main/java/com/rubyhuntersky/indexrebellion/presenters/main/CorrectionBodyViewHolder.kt
+++ app/src/main/java/com/rubyhuntersky/indexrebellion/presenters/main/CorrectionBodyViewHolder.kt
@@ -2,6 +2,7 @@ package com.rubyhuntersky.indexrebellion.presenters.main
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.LinearLayout
 import com.rubyhuntersky.data.report.Correction
 import com.rubyhuntersky.indexrebellion.R
 import kotlinx.android.synthetic.main.view_corrections_body.view.*
@@ -15,6 +16,15 @@ class CorrectionBodyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemVie
                     correctionHeadingTextView.text = correction.assetSymbol.toString()
                     correctionCircleView.setBackgroundResource(R.drawable.ic_check_circle_black_24dp)
                     correctionActionButton.text = context.getString(R.string.hold)
+                    if (correction.weight == 0.0) {
+                        leftSpace.setWeight(1f)
+                        leftWing.setWeight(0f)
+                        rightWing.setWeight(0f)
+                        rightSpecial.setWeight(0f)
+                        rightSpace.setWeight(1f)
+                    } else {
+
+                    }
                 }
                 is Correction.Buy -> {
                     correctionHeadingTextView.text = correction.assetSymbol.toString()
@@ -28,5 +38,11 @@ class CorrectionBodyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemVie
                 }
             }
         }
+    }
+
+    private fun View.setWeight(weight: Float) {
+        val params = layoutParams as LinearLayout.LayoutParams
+        params.weight = weight
+        layoutParams = params
     }
 }
