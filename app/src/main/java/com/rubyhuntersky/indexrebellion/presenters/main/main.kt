15d1e18bda37ac57230e9a75b6082919a9219352
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-01-09 14:32:58 -0800

    Add CorrectionWeightCalculator

---
+++ app/src/main/java/com/rubyhuntersky/indexrebellion/presenters/main/main.kt
@@ -0,0 +1,55 @@
+package com.rubyhuntersky.indexrebellion.presenters.main
+
+data class CorrectionWeights(
+    val leftSpace: Double, val leftWing: Double,
+    val rightWing: Double, val rightSpecial: Double, val rightSpace: Double
+)
+
+object CorrectionWeightCalculator {
+
+    private const val maxWeight: Double = 1000.0
+    private const val highWeight = maxWeight * 1.0
+
+    fun calculate(highValue: Double, targetValue: Double, actualValue: Double): CorrectionWeights {
+        require(highValue >= 0.0)
+        require(targetValue in 0.0..highValue)
+        require(actualValue in 0.0..highValue)
+        return if (highValue == 0.0) {
+            CorrectionWeights(
+                leftSpace = maxWeight, leftWing = 0.0,
+                rightWing = 0.0, rightSpecial = 0.0, rightSpace = maxWeight
+            )
+        } else {
+            if (targetValue == actualValue) {
+                val weight = (targetValue / highValue) * highWeight
+                CorrectionWeights(
+                    leftSpace = maxWeight - weight,
+                    leftWing = weight,
+                    rightWing = weight,
+                    rightSpecial = 0.0,
+                    rightSpace = maxWeight - weight
+                )
+            } else {
+                val targetWeight = (targetValue / highValue) * highWeight
+                val actualWeight = (actualValue / highValue) * highWeight
+                if (targetValue > actualValue) {
+                    CorrectionWeights(
+                        leftSpace = maxWeight - targetWeight,
+                        leftWing = targetWeight,
+                        rightWing = actualWeight,
+                        rightSpecial = (targetWeight - actualWeight),
+                        rightSpace = maxWeight - targetWeight
+                    )
+                } else {
+                    CorrectionWeights(
+                        leftSpace = maxWeight - targetWeight,
+                        leftWing = targetWeight,
+                        rightWing = targetWeight,
+                        rightSpecial = (actualWeight - targetWeight),
+                        rightSpace = maxWeight - actualWeight
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/test/java/com/rubyhuntersky/indexrebellion/presenters/main/CorrectionWeightCalculatorTest.kt
@@ -0,0 +1,59 @@
+package com.rubyhuntersky.indexrebellion.presenters.main
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class CorrectionWeightCalculatorTest {
+    @Test
+    fun highOfZeroAssignsAllWeightToSpace() {
+
+        val result = CorrectionWeightCalculator.calculate(
+            highValue = 0.0,
+            targetValue = 0.0,
+            actualValue = 0.0
+        )
+        assertEquals(
+            CorrectionWeights(1000.0, 0.0, 0.0, 0.0, 1000.0),
+            result
+        )
+    }
+
+    @Test
+    fun equalValuesAssignsEqualWeightToWings() {
+        val result = CorrectionWeightCalculator.calculate(
+            highValue = .5,
+            targetValue = .25,
+            actualValue = .25
+        )
+        assertEquals(
+            CorrectionWeights(500.0, 500.0, 500.0, 0.0, 500.0),
+            result
+        )
+    }
+
+    @Test
+    fun higherTargetSplitsLeftWingBetweenRightWingAndSpecial() {
+        val result = CorrectionWeightCalculator.calculate(
+            highValue = .5,
+            targetValue = .25,
+            actualValue = .125
+        )
+        assertEquals(
+            CorrectionWeights(500.0, 500.0, 250.0, 250.0, 500.0),
+            result
+        )
+    }
+
+    @Test
+    fun lowerTargetBalancesLeftAndRightWingsWithLeftSpaceEqualToRightWingAndSpecial() {
+        val result = CorrectionWeightCalculator.calculate(
+            highValue = .5,
+            targetValue = .125,
+            actualValue = .25
+        )
+        assertEquals(
+            CorrectionWeights(750.0, 250.0, 250.0, 250.0, 500.0),
+            result
+        )
+    }
+}
