d752c1354262dafb7c8d166513c56c90e47e1bf7
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-02-15 16:19:09 -0800

    Show original text in input only when focused to work around overlap bug

--- app/src/main/java/com/rubyhuntersky/indexrebellion/presenters/cashediting/BackingViewInputLayout.kt
+++ app/src/main/java/com/rubyhuntersky/indexrebellion/presenters/cashediting/BackingViewInputLayout.kt
@@ -24,7 +24,7 @@ class BackingViewInputLayout
     defStyleRes: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr, defStyleRes), ViewBackedDashView.BackingView {
 
-    private var inputLayout: TextInputLayout
+    private var layout: TextInputLayout
 
     init {
         val view = LayoutInflater.from(context).inflate(R.layout.view_input, this, false)
@@ -32,19 +32,25 @@ class BackingViewInputLayout
             view,
             ViewGroup.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
         )
-        inputLayout = view as TextInputLayout
+        layout = view as TextInputLayout
     }
 
     fun render(content: Input) {
-        inputLayout.hint = content.label
-        inputEditText.hint = content.originalText
-        if (inputEditText.text.toString() != content.text) {
-            inputEditText.setText(content.text)
+        layout.hint = content.label
+        editText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                editText.hint = content.originalText
+            } else {
+                editText.hint = null
+            }
+        }
+        if (editText.text.toString() != content.text) {
+            editText.setText(content.text)
         }
         val drawable = (content.icon as? Icon.ResId)?.let {
             resources.getDrawable(it.resId, context.theme)
         }
-        inputEditText.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null)
+        editText.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null)
     }
 
     override val heights: Observable<Int>
@@ -64,7 +70,7 @@ class BackingViewInputLayout
 
     override var onDetached: (() -> Unit)? = null
 
-    private val inputEditText by lazy { inputLayout.inputEditText }
+    private val editText by lazy { layout.inputEditText }
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
