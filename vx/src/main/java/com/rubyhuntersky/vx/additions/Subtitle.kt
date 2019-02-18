df175e5b55b84f618c40dcd48e182792f006b97d
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-02-18 08:10:19 -0800

    Rename TitleSubtitle to TitleSubtitleSight

--- vx/src/main/java/com/rubyhuntersky/vx/additions/Subtitle.kt
+++ vx/src/main/java/com/rubyhuntersky/vx/additions/Subtitle.kt
@@ -1,16 +1,18 @@
 package com.rubyhuntersky.vx.additions
 
-import com.rubyhuntersky.vx.*
-import com.rubyhuntersky.vx.dashes.TextLineSight
+import com.rubyhuntersky.vx.Dash
 import com.rubyhuntersky.vx.dashes.TextLineDash
+import com.rubyhuntersky.vx.dashes.TextLineSight
 import com.rubyhuntersky.vx.dashes.TextStyle
 import com.rubyhuntersky.vx.dashes.TitleDash
+import com.rubyhuntersky.vx.transform
 
-operator fun TitleDash.plus(@Suppress("UNUSED_PARAMETER") subtitle: Subtitle): Dash<TitleSubtitle, Nothing> =
+operator fun TitleDash.plus(@Suppress("UNUSED_PARAMETER") subtitle: Subtitle): Dash<TitleSubtitleSight, Nothing> =
     TitleAtopSubtitleDash + Bottom(SubtitleBelowTitleDash) { Pair(it.title, it.subtitle) }
 
 object Subtitle
-data class TitleSubtitle(val title: String, val subtitle: String)
+
+data class TitleSubtitleSight(val title: String, val subtitle: String)
 
 private object TitleAtopSubtitleDash :
     Dash<String, Nothing> by TextLineDash().transform({
